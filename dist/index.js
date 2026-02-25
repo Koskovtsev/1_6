665596177b7ef3fab2ea2e6ab746ea7446363360
Lenovo <drvirus15@gmail.com>
2026-02-25 21:58:46 +0200

    practics

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,62 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('hi');
-console.log('Привіт???');
-console.log('Good Day');
+function getFirstWord(a) {
+    return a.split(/ +/)[0]?.length ?? 0;
+}
+function test() {
+    const a = {
+        name: ['V', 'a', 's', 'y', 'a'],
+        surname: ['P', 'u', 'p', 'k', 'i', 'n'],
+    };
+    console.log(getUserNamings(a));
+}
+function getUserNamings(a) {
+    return {
+        fullname: a.name + " " + a.surname,
+        initials: a.name[0] + "." + a.surname[0]
+    };
+}
+// <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining>
+function getAllProductNames(a) {
+    return a?.products?.map(prod => prod?.name) || [];
+}
+// easy way is using 'as' keyword
+// hard way is ?...
+// function hey(a: { name: Function, cuteness?: number, coolness?: number }) {
+//     return "hey! i'm " + a.name();
+// }
+// hey({name: () => "roman", cuteness: 100})
+// hey({name: () => "vasyl", coolness: 100})
+class Pet {
+    surname;
+    constructor(name) {
+        this.surname = name;
+    }
+    name() {
+        return this.surname;
+    }
+    ;
+}
+class Cat extends Pet {
+    isFast;
+    constructor(name, isFast) {
+        super(name);
+        this.isFast = isFast;
+    }
+}
+class Dog extends Pet {
+    age;
+    constructor(name, age) {
+        super(name);
+        this.age = age;
+    }
+}
+function hey(abstractPet) {
+    return "hey! i'm " + abstractPet.name();
+}
+let a = new Cat("snizhok", true);
+let b = new Dog("sirko", 333);
+hey(a);
+hey(b);
+console.log('all is OK!');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,73 @@
-console.log('hi');
-console.log('Привіт???');
-console.log('Good Day');
+function getFirstWord(a: string): number {
+    return a.split(/ +/)[0]?.length ?? 0;
+}
+
+
+function test() {
+    const a = {
+        name: ['V', 'a', 's', 'y', 'a'],
+        surname: ['P', 'u', 'p', 'k', 'i', 'n'],
+    }
+    console.log(getUserNamings(a));
+}
+
+function getUserNamings(a: { name: string[], surname: string[] }) {
+    return {
+        fullname: a.name + " " + a.surname,
+        initials: a.name[0] + "." + a.surname[0]
+    };
+}
+
+// <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining>
+function getAllProductNames(a?: { products?: { name: string }[] }) {
+    return a?.products?.map(prod => prod?.name) || [];
+}
+
+// easy way is using 'as' keyword
+// hard way is ?...
+// function hey(a: { name: Function, cuteness?: number, coolness?: number }) {
+//     return "hey! i'm " + a.name();
+// }
+// hey({name: () => "roman", cuteness: 100})
+// hey({name: () => "vasyl", coolness: 100})
+
+class Pet {
+    surname: string;
+   
+    constructor(name: string) {
+        this.surname = name;
+    }
+    name() {
+        return this.surname;
+    };
+}
+
+class Cat extends Pet {
+    isFast: boolean;
+
+    constructor(name: string, isFast: boolean) {
+        super(name);
+        this.isFast = isFast;
+    }
+}
+
+
+class Dog extends Pet {
+    age: number;
+
+    constructor(name: string, age: number) {
+        super(name);
+        this.age = age;
+    }
+}
+
+function hey(abstractPet: Pet) {
+    return "hey! i'm " + abstractPet.name();
+}
+let a = new Cat("snizhok", true)
+let b = new Dog("sirko", 333)
+hey(a)
+hey(b)
+
+
+console.log('all is OK!');
